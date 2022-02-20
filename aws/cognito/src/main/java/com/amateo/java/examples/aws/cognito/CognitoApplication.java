3056cfbff5abdc754336d7d3efa8d835c06bae1b
alejandro.mateo <alejandro.mateo@clearpay.com>
2022-02-20 08:01:23 +0100

    initial commit for aws/cognito

---
+++ aws/cognito/src/main/java/com/amateo/java/examples/aws/cognito/CognitoApplication.java
@@ -0,0 +1,15 @@
+package com.amateo.java.examples.aws.cognito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CognitoApplication.class, args);
+
+    }
+
+}
